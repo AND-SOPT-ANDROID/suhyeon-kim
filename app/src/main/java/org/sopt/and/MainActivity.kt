a7c69fc5633f2ba022a33d8dd8fa682bd53aced3
gitsuhyun <suhyun10201@naver.com>
2024-10-23 15:50:18 +0900

    #4 [feat] : 하단 Bottom Navigation 구현

--- app/src/main/java/org/sopt/and/MainActivity.kt
+++ app/src/main/java/org/sopt/and/MainActivity.kt
@@ -1,43 +1,188 @@
 package org.sopt.and
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import org.sopt.and.presentation.screens.Routes
+import org.sopt.and.presentation.screens.home.HomeScreen
 import org.sopt.and.presentation.screens.login.LoginScreen
 import org.sopt.and.presentation.screens.mypage.MyScreen
+import org.sopt.and.presentation.screens.search.SearchScreen
 import org.sopt.and.presentation.screens.signup.SignUpScreen
+import org.sopt.and.presentation.utils.noRippleClickable
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 import org.sopt.and.viewmodel.LoginViewModel
 import org.sopt.and.viewmodel.MyViewModel
 import org.sopt.and.viewmodel.SignUpViewModel
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ANDANDROIDTheme {
                 val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = Routes.SignUp.screen,
+                var isBottomAppBarVisible by remember { mutableStateOf(true) }
+                val selected = remember {
+                    mutableStateOf("홈")
+                }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        if (isBottomAppBarVisible) {
+                            BottomAppBar(
+                                modifier = Modifier.height(110.dp),
+                                containerColor = Color.Black,
+                            ) {
+                                NavItem(
+                                    icon = {
+                                        Icon(
+                                            imageVector = Icons.Outlined.Home,
+                                            contentDescription = "홈",
+                                            modifier = Modifier.padding(bottom = 5.dp),
+                                            tint = if (selected.value == "홈") Color.White else Color.Gray
+                                        )
+                                    },
+                                    label = "홈",
+                                    selected = selected,
+                                    modifier = Modifier
+                                        .noRippleClickable {
+                                            selected.value = "홈"
+                                            navController.navigate(Routes.Home.screen) {
+                                                popUpTo(0)
+                                            }
+                                        }
+                                )
+                                NavItem(
+                                    icon = {
+                                        Icon(
+                                            imageVector = Icons.Outlined.Search,
+                                            contentDescription = "검색",
+                                            modifier = Modifier.padding(bottom = 5.dp),
+                                            tint = if (selected.value == "검색") Color.White else Color.Gray
+                                        )
+                                    },
+                                    label = "검색",
+                                    selected = selected,
+                                    modifier = Modifier
+                                        .noRippleClickable {
+                                            selected.value = "검색"
+                                            navController.navigate(Routes.Search.screen) {
+                                                popUpTo(0)
+                                            }
+                                        }
+                                )
+                                NavItem(
+                                    icon = {
+                                        Image(
+                                            painter = painterResource(id = R.drawable.ic_launcher_background),
+                                            contentDescription = stringResource(R.string.profile),
+                                            modifier = Modifier
+                                                .clip(CircleShape)
+                                                .size(30.dp)
+                                        )
+                                    },
+                                    label = "MY",
+                                    selected = selected,
+                                    modifier = Modifier
+                                        .noRippleClickable {
+                                            selected.value = "MY"
+                                            navController.navigate(Routes.My.screen) {
+                                                popUpTo(0)
+                                            }
+                                        }
+                                )
+                            }
+                        }
+                    }
                 ) {
-                    composable(Routes.Login.screen) {
-                        LoginScreen(navController = navController, viewModel = LoginViewModel())
-                    }
-                    composable(Routes.SignUp.screen) {
-                        SignUpScreen(navController = navController, viewModel = SignUpViewModel())
-                    }
-                    composable(Routes.My.screen) {
-                        MyScreen(navController = navController, viewModel = MyViewModel())
+                    NavHost(
+                        navController = navController,
+                        startDestination = Routes.Login.screen,
+                    ) {
+                        composable(Routes.Login.screen) {
+                            isBottomAppBarVisible = false
+                            LoginScreen(navController = navController, viewModel = LoginViewModel())
+                        }
+                        composable(Routes.SignUp.screen) {
+                            isBottomAppBarVisible = false
+                            SignUpScreen(
+                                navController = navController,
+                                viewModel = SignUpViewModel()
+                            )
+                        }
+                        composable(Routes.Home.screen) {
+                            isBottomAppBarVisible = true
+                            HomeScreen(navController = navController, viewModel = MyViewModel())
+                        }
+                        composable(Routes.Search.screen) {
+                            isBottomAppBarVisible = true
+                            SearchScreen(navController = navController, viewModel = MyViewModel())
+                        }
+                        composable(Routes.My.screen) {
+                            isBottomAppBarVisible = true
+                            MyScreen(navController = navController, viewModel = MyViewModel())
+                        }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun RowScope.NavItem(
+    icon: @Composable () -> Unit,
+    label: String,
+    selected: MutableState<String>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .weight(1f),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        icon()
+        Text(
+            text = label,
+            color = if (selected.value == label) Color.White else Color.Gray
+        )
     }
 }
