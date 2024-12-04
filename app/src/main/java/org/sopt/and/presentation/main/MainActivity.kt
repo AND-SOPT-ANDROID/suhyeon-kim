9baff1bfa1ed675f13ffd6c98262cba7e7086480
gitsuhyun <suhyun10201@naver.com>
2024-12-04 15:28:38 +0900

    #10 [feat] : hilt 세팅

--- app/src/main/java/org/sopt/and/presentation/main/MainActivity.kt
+++ app/src/main/java/org/sopt/and/presentation/main/MainActivity.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 import org.sopt.and.utils.currentRoute
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
